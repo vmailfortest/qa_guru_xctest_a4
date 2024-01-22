0091519f827990c0d38b7af1429c941db2e4e89b
Vlad Zinger <vladislavzinger@gmail.com>
2024-01-23 01:36:38 +0700

    Added select testplan and device

--- CalculatorUITests/CalculatorUITests.swift
+++ CalculatorUITests/CalculatorUITests.swift
@@ -11,7 +11,9 @@ final class CalculatorUITests: XCTestCase {
     override func tearDownWithError() throws {}
 
     func testExample() throws {
-        app.launch()
+        step("Запуск приложения") {
+            app.launch()
+        }
 
         let attachment = XCTAttachment(screenshot: app.screenshot())
         attachment.name = "Launch screen"
@@ -19,19 +21,24 @@ final class CalculatorUITests: XCTestCase {
         add(attachment)
     }
     
-    func testFail() throws {
-        app.launch()
-        XCTFail("Failure from test")
-    }
-    
     func testSum() throws {
-        app.launch()
+        step("Запуск приложения") {
+            app.launch()
+        }
         let button2 = app/*@START_MENU_TOKEN@*/.staticTexts["2"]/*[[".buttons[\"2\"].staticTexts[\"2\"]",".staticTexts[\"2\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.firstMatch
         button2.tap()
         app/*@START_MENU_TOKEN@*/.staticTexts["+"]/*[[".buttons[\"+\"].staticTexts[\"+\"]",".staticTexts[\"+\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
         button2.tap()
         app.buttons["="].tap()
         XCTAssert(app.buttons["resultString"].staticTexts["4"].exists)
+    }
+    
+    func testSkip() throws {
+        step("Запуск приложения") {
+            app.launch()
+        }
+        
+        try XCTSkipIf(true, "Сервер не отвечает")
     }
 
 }
